104796f7d126601fcb75a044d1f023763970a292
Matthias Schlaipfer <m.schlaipfer@gmail.com>
2019-01-05 22:13:15 +0100

    Changed directory structure to use stow

--- zsh/.zsh_user_cfg.sh
+++ zsh/.zsh_user_cfg.sh
@@ -2,7 +2,7 @@ export PATH="$PATH:/usr/sbin"
 export JAVA_HOME="/usr/lib/jvm/jdk-8-oracle-x64"
 export JDK_HOME="/usr/lib/jvm/jdk-8-oracle-x64"
 
-#export TERM="xterm-256color"
+#export TERM="kitty"
 
 if [ -n "$DISPLAY" ]; then
   xset b off
