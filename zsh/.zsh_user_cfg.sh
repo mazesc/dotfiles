cb8b5c457eb68a126800ccc4fd1d5071c3bb0bca
Matthias Schlaipfer <m.schlaipfer@gmail.com>
2019-05-25 21:54:39 -0700

    fix end/home not working in zsh

--- zsh/.zsh_user_cfg.sh
+++ zsh/.zsh_user_cfg.sh
@@ -4,6 +4,11 @@ export JDK_HOME="/usr/lib/jvm/jdk-8-oracle-x64"
 
 #export TERM="kitty"
 
+#test -r /home/matthias/.opam/opam-init/init.zsh && . /home/matthias/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
+
+bindkey  "^[[H"   beginning-of-line
+bindkey  "^[[F"   end-of-line
+
 if [ -n "$DISPLAY" ]; then
   xset b off
 fi
